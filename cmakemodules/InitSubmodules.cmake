f176ec87963638b5c3497359072966ec31f247b7
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2020-02-13 17:11:31 +0100

    Add TLX as submodule

---
+++ cmakemodules/InitSubmodules.cmake
@@ -0,0 +1,16 @@
+# find Git
+find_package(Git)
+
+if(GIT_FOUND)
+    # update submodules
+    execute_process(
+        COMMAND "${GIT_EXECUTABLE}" "submodule" "update" "--init" "--recursive"
+        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+        RESULT_VARIABLE GIT_RESULT)
+
+    if(NOT ${GIT_RESULT} EQUAL 0)
+        message(WARNING "Failed to initialize submodules: ${GIT_RESULT}")
+    endif()
+else()
+    message(WARNING "Could not find git -- submodules must be initialized manually")
+endif()
