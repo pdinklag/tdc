727772a0f498f86936837a9fe1cd8108b6698e08
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2020-02-13 17:19:30 +0100

    Add malloc_callback as a library

---
+++ include/tdc/malloc/malloc_callback.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+#include <cstdlib>
+
+namespace tdc {
+namespace malloc_callback {
+    void on_alloc(size_t);
+    void on_free(size_t);
+}}
+
+extern "C" void* __libc_malloc(size_t);
+extern "C" void  __libc_free(void*);
+extern "C" void* __libc_realloc(void*, size_t);
---
+++ src/malloc/malloc.cpp
@@ -0,0 +1,76 @@
+#include <tdc/malloc/malloc_callback.hpp>
+
+#include <cstring>
+
+constexpr size_t MEMBLOCK_MAGIC = 0xFEDCBA9876543210;
+
+struct block_header_t {
+    size_t magic;
+    size_t size;
+};
+
+inline bool is_managed(block_header_t* block) {
+    return (block->magic == MEMBLOCK_MAGIC);
+}
+
+extern "C" void* malloc(size_t size) {
+    if(!size) return NULL;
+
+    void *ptr = __libc_malloc(size + sizeof(block_header_t));
+    if(!ptr) return ptr; // malloc failed
+
+    auto block = (block_header_t*)ptr;
+    block->magic = MEMBLOCK_MAGIC;
+    block->size = size;
+
+    tdc::malloc_callback::on_alloc(size);
+
+    return (char*)ptr + sizeof(block_header_t);
+}
+
+extern "C" void free(void* ptr) {
+    if(!ptr) return;
+
+    auto block = (block_header_t*)((char*)ptr - sizeof(block_header_t));
+    if(is_managed(block)) {
+        tdc::malloc_callback::on_free(block->size);
+        __libc_free(block);
+    } else {
+        __libc_free(ptr);
+    }
+}
+
+extern "C" void* realloc(void* ptr, size_t size) {
+    if(!size) {
+        free(ptr);
+        return NULL;
+    } else if(!ptr) {
+        return malloc(size);
+    } else {
+        auto block = (block_header_t*)((char*)ptr - sizeof(block_header_t));
+        if(is_managed(block)) {
+            size_t old_size = block->size;
+            void *new_ptr = __libc_realloc(block, size + sizeof(block_header_t));
+
+            auto new_block = (block_header_t*)new_ptr;
+            new_block->magic = MEMBLOCK_MAGIC; // just making sure
+            new_block->size = size;
+
+            tdc::malloc_callback::on_free(old_size);
+            tdc::malloc_callback::on_alloc(size);
+
+            return (char*)new_ptr + sizeof(block_header_t);
+        } else {
+            return __libc_realloc(ptr, size);
+        }
+    }
+}
+
+extern "C" void* calloc(size_t num, size_t size) {
+    size *= num;
+    if(!size) return NULL;
+
+    void* ptr = malloc(size);
+    memset(ptr, 0, size);
+    return ptr;
+}
