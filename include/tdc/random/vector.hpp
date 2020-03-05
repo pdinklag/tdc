50884fcd5b9068abc4298821485e61694a456b4f
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2020-03-05 08:30:42 +0100

    Allow random vectors of bool

--- include/tdc/random/vector.hpp
+++ include/tdc/random/vector.hpp
@@ -1,10 +1,19 @@
 #pragma once
 
+#include <random>
 #include <vector>
+
 #include "seed.hpp"
 
 namespace tdc {
 namespace random {
+
+/// \cond INTERNAL
+
+// this is used to map bool to uint8_t, because you cannot have a uniform_int_distribution for the bool type
+template<typename T> struct map_type { using value = T; };
+template<> struct map_type<bool> { using value = uint8_t; };
+/// \endcond
 
 /// \brief Generates a vector of random numbers between 0 and the given maximum (inclusive).
 /// \tparam the number type
@@ -18,7 +27,7 @@ std::vector<T> vector(const size_t num, const T max, const uint64_t seed = DEFAU
 
     // seed
     std::default_random_engine gen(seed);
-    std::uniform_int_distribution<T> dist(0, max);
+    std::uniform_int_distribution<typename map_type<T>::value> dist(0, max);
 
     // generate
     for(size_t i = 0; i < num; i++) {
