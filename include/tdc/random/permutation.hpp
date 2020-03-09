48b72efe85b00a26d8856bf857476350184da23a
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2020-03-09 10:33:39 +0100

    Random permutation utility

---
+++ include/tdc/random/permutation.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <random>
+#include <vector>
+
+#include "seed.hpp"
+
+namespace tdc {
+namespace random {
+
+/// \brief Generates a random permutation of a given universe with near-uniform distribution.
+///
+/// This is based on an article by Jeff Preshing (https://preshing.com/20121224/how-to-generate-a-sequence-of-unique-random-integers), who describes how to generate random permutations of 32-bit numbers using quadratic residues of primes,
+/// modified to support an arbitrary universe size.
+class Permutation {
+private:
+    // finds the largest prime p less than or equal to x that satisfies p = (3 mod 4)
+    static uint64_t prev_prime_3mod4(const uint64_t x);
+
+    const uint64_t m_universe;
+    const uint64_t m_seed;
+    const uint64_t m_prime;
+
+    // permute the given number
+    uint64_t permute(const uint64_t x) const;
+
+public:
+    /// \brief Initializes a permutation.
+    /// \param universe the universe size
+    /// \param seed the random seed
+    Permutation(const uint64_t universe, const uint64_t seed = DEFAULT_SEED);
+
+    /// \brief Permutes the given number.
+    /// \param i the number to permute, must be within the universe
+    uint64_t operator()(const uint64_t i) const;
+
+    /// \brief Permutes a set of increasing numbers and returns the permuted numbers as a vector.
+    /// \param num the number of numbers to permute
+    /// \param start the first number to permute
+    std::vector<uint64_t> vector(const size_t num, const uint64_t start = 0ULL) const;
+};
+
+}} // namespace tdc::random
---
+++ src/random/permutation.cpp
@@ -0,0 +1,45 @@
+#include <tdc/random/permutation.hpp>
+#include <tdc/math/prime.hpp>
+
+using namespace tdc::random;
+
+// provides a decent distribution of 64 bits
+constexpr uint64_t SHUFFLE1 = 0x9696594B6A5936B2ULL;
+constexpr uint64_t SHUFFLE2 = 0xD2165B4B66592AD6ULL;
+
+uint64_t Permutation::prev_prime_3mod4(const uint64_t x) {
+    uint64_t p = math::prime_predecessor(x);
+    while((p & 3ULL) != 3ULL) {
+        p = math::prime_predecessor(p - 1);
+    }
+    return p;
+}
+
+uint64_t Permutation::permute(const uint64_t x) const {
+    if(x >= m_prime) {
+        // map numbers in gap to themselves - shuffling will take care of this
+        return x;
+    } else {
+        // use quadratic residue
+        const uint64_t r = (uint64_t)((__uint128_t)x * (__uint128_t)x) % m_prime;
+        return (x <= (m_prime >> 1ULL)) ? r : m_prime - r;
+    }
+}
+
+Permutation::Permutation(const uint64_t universe, const uint64_t seed)
+    : m_universe(universe),
+      m_prime(prev_prime_3mod4(universe)),
+      m_seed((seed ^ SHUFFLE1) ^ SHUFFLE2) {
+}
+
+uint64_t Permutation::operator()(const uint64_t i) const {
+    return permute((m_seed + permute(i)) % m_universe);
+}
+
+std::vector<uint64_t> Permutation::vector(const size_t num, const uint64_t start) const {
+    std::vector<uint64_t> vec(num);
+    for(size_t i = 0; i < num; i++) {
+        vec[i] = (*this)(start + i);
+    }
+    return vec;
+}
