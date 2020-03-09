c823048d252918752c1e07cd838c2de877c0a88d
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2020-03-09 09:04:52 +0100

    FixedWidthIntVector instantiations

---
+++ src/vec/fixed_width_int_vector.cpp
@@ -0,0 +1,32 @@
+#include <tdc/vec/fixed_width_int_vector.hpp>
+
+using namespace tdc::vec;
+
+template class FixedWidthIntVector_<2>;
+template class FixedWidthIntVector_<3>;
+template class FixedWidthIntVector_<4>;
+template class FixedWidthIntVector_<5>;
+template class FixedWidthIntVector_<6>;
+template class FixedWidthIntVector_<7>;
+template class FixedWidthIntVector_<9>;
+template class FixedWidthIntVector_<10>;
+template class FixedWidthIntVector_<11>;
+template class FixedWidthIntVector_<12>;
+template class FixedWidthIntVector_<13>;
+template class FixedWidthIntVector_<14>;
+template class FixedWidthIntVector_<15>;
+template class FixedWidthIntVector_<17>;
+template class FixedWidthIntVector_<18>;
+template class FixedWidthIntVector_<19>;
+template class FixedWidthIntVector_<20>;
+template class FixedWidthIntVector_<21>;
+template class FixedWidthIntVector_<22>;
+template class FixedWidthIntVector_<23>;
+template class FixedWidthIntVector_<24>;
+template class FixedWidthIntVector_<25>;
+template class FixedWidthIntVector_<26>;
+template class FixedWidthIntVector_<27>;
+template class FixedWidthIntVector_<28>;
+template class FixedWidthIntVector_<29>;
+template class FixedWidthIntVector_<30>;
+template class FixedWidthIntVector_<31>;
