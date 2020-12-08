14b2d8ad27c2132f178c830cf295cfdc83d0cd03
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2020-12-08 08:42:59 +0100

    Allow stating root dirs in MPFR finder

--- cmakemodules/FindMPFR.cmake
+++ cmakemodules/FindMPFR.cmake
@@ -1,12 +1,14 @@
 # Find GNU Multiple Precision Arithmetic Library
 include(FindPackageHandleStandardArgs)
 
-find_path(GMP_INCLUDE_DIR gmp.h)
-find_library(GMP_LIBRARY gmp)
+set(GMP_ROOT_DIR "" CACHE PATH "Path to GMP")
+find_path(GMP_INCLUDE_DIR gmp.h PATHS ${GMP_ROOT_DIR})
+find_library(GMP_LIBRARY gmp PATHS ${GMP_ROOT_DIR} PATH_SUFFIXES ".libs")
 find_package_handle_standard_args(GMP DEFAULT_MSG GMP_INCLUDE_DIR GMP_LIBRARY)
 
-find_path(MPFR_INCLUDE_DIR mpfr.h)
-find_library(MPFR_LIBRARY mpfr)
+set(MPFR_ROOT_DIR "" CACHE PATH "Path to MPFR")
+find_path(MPFR_INCLUDE_DIR mpfr.h PATHS ${MPFR_ROOT_DIR} PATH_SUFFIXES "src")
+find_library(MPFR_LIBRARY mpfr PATHS ${MPFR_ROOT_DIR} PATH_SUFFIXES "src/.libs")
 find_package_handle_standard_args(MPFR DEFAULT_MSG MPFR_INCLUDE_DIR MPFR_LIBRARY)
 
 if(GMP_FOUND AND MPFR_FOUND)
