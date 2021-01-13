8c147c497971e7bf670af4be0c0543bfda1953a4
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2021-01-13 12:32:47 +0100

    Hybrid sampling buckets: save space by using just a single pointer

--- include/tdc/pred/dynamic/buckets/buckets.hpp
+++ include/tdc/pred/dynamic/buckets/buckets.hpp
@@ -167,33 +167,47 @@ struct bucket_hybrid : bucket_base<b_wordl> {
   using base = bucket_base<b_wordl>;
   using suffix_t = typename base::suffix_t;
   using list_t = typename base::list_t;
+  using bv_t = std::bitset<base::MAX_NUM>;
     
   static constexpr size_t upper_threshhold = base::template hybrid_threshold<key_t>();
   static constexpr size_t lower_threshhold = base::template hybrid_threshold<key_t>() / 2;
-  suffix_t m_size = 0;
+  
   const uint64_t m_prefix;
   uint64_t m_prev_pred = 0;
   bucket_hybrid *m_next_b = nullptr;
+  
+  void* m_ptr; // points to a list_t or a bv_t, the least significant bit tells us which  
+  inline bool m_is_list() const { return !((uint64_t)m_ptr & 1); }
+  inline list_t* m_list() { return ((list_t*)m_ptr); }
+  inline bv_t* m_bv() { return ((bv_t*)((uint64_t)m_ptr - 1)); }
+  inline const list_t* m_list() const { return ((const list_t*)m_ptr); }
+  inline const bv_t* m_bv() const { return ((const bv_t*)((uint64_t)m_ptr - 1)); }
 
-  bool m_is_list = true;
-  list_t m_list;
-  std::bitset<base::MAX_NUM>* m_bits = nullptr;
+  suffix_t m_size = 0;
 
   bucket_hybrid(uint64_t prefix, uint64_t prev_pred, bucket_hybrid *next_b, suffix_t suf)
       : m_prefix(prefix), m_prev_pred(prev_pred), m_next_b(next_b) {
+
+    m_ptr = (void*)new list_t(); // start as a list
     set(suf);
   }
 
   ~bucket_hybrid() {
-    delete m_bits;
+    if(m_is_list()) {
+      delete m_list();
+    } else {
+      delete m_bv();
+    }
   }
   uint64_t get_min() {
     assert(m_size > 0);
-    if (m_is_list) {
-      return (m_prefix << b_wordl) + *std::min_element(std::begin(m_list), std::end(m_list));
+    if (m_is_list()) {
+      auto& list = *m_list();
+      return (m_prefix << b_wordl) + *std::min_element(std::begin(list), std::end(list));
     } else {
+      auto& bv = *m_bv();
       size_t i = 0;
-      while (!(*m_bits)[i]) {
+      while (!bv[i]) {
         ++i;
       }
       return (m_prefix << b_wordl) + i;
@@ -201,29 +215,30 @@ struct bucket_hybrid : bucket_base<b_wordl> {
   }
 
   void set(const uint64_t suf) {
-    if (m_is_list) {
-      m_list.push_back(suf);
+    if (m_is_list()) {
+      m_list()->push_back(suf);
     } else {
-      (*m_bits)[suf] = true;
+      (*m_bv())[suf] = true;
     }
 
     ++m_size;
     if (tdc_likely(m_next_b != nullptr)) {
       m_next_b->m_prev_pred = std::max((m_prefix << b_wordl) + suf, m_next_b->m_prev_pred);
     }
-    if (m_is_list && (m_size > upper_threshhold)) {
+    if (m_is_list() && (m_size > upper_threshhold)) {
       rebuild();
     }
   }
 
   void remove(const uint64_t suf) {
-    if (m_is_list) {
-      auto p = std::find(m_list.begin(), m_list.end(), suf);
-      assert(p != m_list.end());
-      *p = (suffix_t)m_list.back();
-      m_list.pop_back();
+    if (m_is_list()) {
+      auto& list = *m_list();
+      auto p = std::find(list.begin(), list.end(), suf);
+      assert(p != list.end());
+      *p = (suffix_t)list.back();
+      list.pop_back();
     } else {
-      (*m_bits)[suf] = false;
+      (*m_bv())[suf] = false;
     }
 
     --m_size;
@@ -233,7 +248,7 @@ struct bucket_hybrid : bucket_base<b_wordl> {
         m_next_b->m_prev_pred = predecessor((m_prefix << b_wordl) + suf);
       }
     }
-    if (!m_is_list && (m_size < lower_threshhold)) {
+    if (!m_is_list() && (m_size < lower_threshhold)) {
       rebuild();
     }
   }
@@ -244,21 +259,23 @@ struct bucket_hybrid : bucket_base<b_wordl> {
 
   uint64_t predecessor(int64_t key) const {
     key = base::prefix(key) != m_prefix ? base::SUFFIX_MAX : base::suffix(key);
-    if (m_is_list) {
-      auto p = std::find_if(m_list.begin(), m_list.end(), [key](const uint64_t x) { return x <= key; });
-      if (p == m_list.end()) {
+    if (m_is_list()) {
+      auto& list = *m_list();
+      auto p = std::find_if(list.begin(), list.end(), [key](const uint64_t x) { return x <= key; });
+      if (p == list.end()) {
         return m_prev_pred;
       }
       suffix_t max_pred = *p;
-      for (; p != m_list.end(); ++p) {
+      for (; p != list.end(); ++p) {
         if ((*p) <= key) {
           max_pred = std::max((suffix_t)*p, max_pred);
         }
       }
       return (m_prefix << b_wordl) + max_pred;
     } else {
+      auto& bv = *m_bv();
       for (; key >= 0; --key) {
-        if ((*m_bits)[key]) {
+        if (bv[key]) {
           return (m_prefix << b_wordl) + key;
         }
       }
@@ -268,28 +285,27 @@ struct bucket_hybrid : bucket_base<b_wordl> {
 
   void
   rebuild() {
-    if (m_is_list) {
-      m_is_list = false;
+    if (m_is_list()) {
       m_size = 0;
-      m_bits = new std::bitset<base::MAX_NUM>; //std::vector<bool>(1ULL << b_wordl);
-      for (suffix_t key : m_list) {
+      auto plist = m_list();
+      m_ptr = (void*)((uint64_t)new bv_t() | 1); // set the LSB to tell that this is a bit vector
+      for (suffix_t key : *plist) {
         set(key);
       }
-      // this frees the memory allocated by the vector
-      list_t().swap(m_list);
+      delete plist;
     } else {
-      m_is_list = true;
+      auto pbv = m_bv();
+      m_ptr = (void*)new list_t(m_size);
       m_size = 0;
       for (size_t i = 0; i < base::MAX_NUM; ++i) {
-        if ((*m_bits)[i]) {
+        if ((*pbv)[i]) {
           set(i);
         }
       }
-      delete m_bits;
-      m_bits = nullptr;
+      delete pbv;
     }
   }
-};
+} __attribute__((__packed__));
 
 template <typename key_t, uint8_t b_wordl>
 struct map_bucket_bv : bucket_base<b_wordl> {
@@ -396,61 +412,83 @@ struct map_bucket_hybrid : bucket_base<b_wordl> {
   using base = bucket_base<b_wordl>;
   using suffix_t = typename base::suffix_t;
   using list_t = typename base::list_t;
+  using bv_t = std::bitset<base::MAX_NUM>;
     
   static constexpr size_t upper_threshhold = base::template hybrid_threshold<key_t>();
   static constexpr size_t lower_threshhold = base::template hybrid_threshold<key_t>() / 2;
+
+  void* m_ptr; // points to a list_t or a bv_t, the least significant bit tells us which  
+  inline bool m_is_list() const { return !((uint64_t)m_ptr & 1); }
+  inline list_t* m_list() { return ((list_t*)m_ptr); }
+  inline bv_t* m_bv() { return ((bv_t*)((uint64_t)m_ptr - 1)); }
+  inline const list_t* m_list() const { return ((const list_t*)m_ptr); }
+  inline const bv_t* m_bv() const { return ((const bv_t*)((uint64_t)m_ptr - 1)); }
+
   suffix_t m_size = 0;
-  bool m_is_list = true;
-  list_t m_list;
-  std::bitset<base::MAX_NUM> *m_bits = nullptr;
 
   map_bucket_hybrid() {
+    m_ptr = (void*)new list_t(); // start as a list
   }
 
   map_bucket_hybrid(suffix_t suf) {
+    m_ptr = (void*)new list_t(); // start as a list
     set(suf);
   }
 
+  map_bucket_hybrid(const map_bucket_hybrid& other) = delete;
+  map_bucket_hybrid(map_bucket_hybrid&& other) = delete;
+  map_bucket_hybrid& operator=(const map_bucket_hybrid& other) = delete;
+  map_bucket_hybrid& operator=(map_bucket_hybrid&& other) = delete;
+
   ~map_bucket_hybrid() {
-    delete m_bits;
+    if(m_ptr) {
+      if(m_is_list()) {
+        delete m_list();
+      } else {
+        delete m_bv();
+      }
+      m_ptr = nullptr;
+    }
   }
 
   uint64_t get_min() {
     assert(m_size > 0);
-    if (m_is_list) {
-      return *std::min_element(std::begin(m_list), std::end(m_list));
+    if (m_is_list()) {
+      auto& list = *m_list();
+      return *std::min_element(std::begin(list), std::end(list));
     } else {
       size_t suf = 0;
-      while (!(*m_bits)[suf]) {
+      while (!(*m_bv())[suf]) {
         ++suf;
       }
       return suf;
     }
   }
   void set(const uint64_t suf) {
-    if (m_is_list) {
-      m_list.push_back(suf);
+    if (m_is_list()) {
+      m_list()->push_back(suf);
     } else {
-      (*m_bits)[suf] = true;
+      (*m_bv())[suf] = true;
     }
     ++m_size;
-    if (m_is_list && (m_size > upper_threshhold)) {
+    if (m_is_list() && (m_size > upper_threshhold)) {
       rebuild();
     }
   }
 
   void remove(const uint64_t suf) {
-    if (m_is_list) {
-      auto p = std::find(m_list.begin(), m_list.end(), suf);
-      assert(p != m_list.end());
-      *p = (suffix_t)m_list.back();
-      m_list.pop_back();
+    if (m_is_list()) {
+      auto& list = *m_list();
+      auto p = std::find(list.begin(), list.end(), suf);
+      assert(p != list.end());
+      *p = (suffix_t)list.back();
+      list.pop_back();
     } else {
-      (*m_bits)[suf] = false;
+      (*m_bv())[suf] = false;
     }
 
     --m_size;
-    if (!m_is_list && (m_size < lower_threshhold)) {
+    if (!m_is_list() && (m_size < lower_threshhold)) {
       rebuild();
     }
   }
@@ -460,21 +498,23 @@ struct map_bucket_hybrid : bucket_base<b_wordl> {
   }
 
   KeyResult<uint64_t> predecessor(int64_t suf) const {
-    if (m_is_list) {
-      auto p = std::find_if(m_list.begin(), m_list.end(), [suf](const uint64_t x) { return x <= suf; });
-      if (p == m_list.end()) {
+    if (m_is_list()) {
+      auto& list = *m_list();
+      auto p = std::find_if(list.begin(), list.end(), [suf](const uint64_t x) { return x <= suf; });
+      if (p == list.end()) {
         return {false, 0};
       }
       suffix_t max_pred = *p;
-      for (; p != m_list.end(); ++p) {
+      for (; p != list.end(); ++p) {
         if ((*p) <= suf) {
           max_pred = std::max((suffix_t)*p, max_pred);
         }
       }
       return {true, max_pred};
     } else {
+      auto& bv = *m_bv();
       for (; suf >= 0; --suf) {
-        if ((*m_bits)[suf]) {
+        if (bv[suf]) {
           return {true, static_cast<uint64_t>(suf)};
         }
       }
@@ -483,28 +523,27 @@ struct map_bucket_hybrid : bucket_base<b_wordl> {
   }
 
   void rebuild() {
-    if (m_is_list) {
-      m_is_list = false;
+    if (m_is_list()) {
       m_size = 0;
-      m_bits = new std::bitset<base::MAX_NUM>;
-      for (suffix_t key : m_list) {
+      auto plist = m_list();
+      m_ptr = (void*)((uint64_t)new bv_t() | 1); // set the LSB to tell that this is a bit vector
+      for (suffix_t key : *plist) {
         set(key);
       }
-      // this frees the memory allocated by the vector
-      list_t().swap(m_list);
+      delete plist;
     } else {
-      m_is_list = true;
+      auto pbv = m_bv();
+      m_ptr = (void*)new list_t(m_size);
       m_size = 0;
       for (size_t i = 0; i < base::MAX_NUM; ++i) {
-        if ((*m_bits)[i]) {
+        if ((*pbv)[i]) {
           set(i);
         }
       }
-      delete m_bits;
-      m_bits = nullptr;
+      delete pbv;
     }
   }
-};  // namespace dynamic
+} __attribute__((__packed__));
 
 }  // namespace dynamic
 }  // namespace pred
