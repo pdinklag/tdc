2b315cfdb4417a0eadbd15bf763760b104d56ef5
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2021-02-26 11:04:03 +0100

    minor CountMin updates

--- include/tdc/sketch/count_min.hpp
+++ include/tdc/sketch/count_min.hpp
@@ -11,14 +11,18 @@
 #include <tdc/hash/function.hpp>
 #include <tdc/math/prime.hpp>
 #include <tdc/random/permutation.hpp>
+#include <tdc/util/index.hpp>
 
 namespace tdc {
 namespace sketch {
 
-/// \brief An implementation of the count-min sketch by Cormode & Muthukrishnan, 2004
-template<typename key_t, typename count_t = size_t>
-requires std::unsigned_integral<count_t> && std::totally_ordered<count_t>
+/// \brief An implementation of the count-min sketch by Cormode & Muthukrishnan, 2004.
+template<typename key_t, typename _count_t = index_t>
+requires std::unsigned_integral<_count_t> && std::totally_ordered<_count_t>
 class CountMin {
+public:
+    using count_t = _count_t;
+
 private:
     size_t m_num_rows, m_num_cols;
     std::vector<hash::Multiplicative> m_hash;
@@ -29,6 +33,13 @@ private:
     }
 
 public:
+    CountMin() : m_num_rows(0), m_num_cols(0) {
+    }
+
+    /// \brief Constructor.
+    /// \param num_cols the number of columns
+    /// \param num_rows the number of rows
+    /// \param seed the random seed for hash function selection
     CountMin(size_t num_cols, size_t num_rows, uint64_t seed = random::DEFAULT_SEED) : m_num_rows(num_rows), m_num_cols(num_cols) {
         // initialize hash functions and data rows
         assert(m_num_rows <= math::NUM_POOL_PRIMES);
@@ -42,17 +53,43 @@ public:
             m_data.emplace_back(std::vector<count_t>(m_num_cols, 0));
         }
     }
+    
+    CountMin(const CountMin& other) = default;
+    CountMin(CountMin&& other) = default;
+    
+    CountMin& operator=(const CountMin& other) = default;
+    CountMin& operator=(CountMin&& other) = default;
 
+    /// \brief Processes a key.
+    /// \param key the key in question
+    /// \param c the number of times to count the key
     void process(const key_t& key, count_t c = count_t(1)) {
         for(size_t i = 0; i < m_num_rows; i++) {
-            m_data[i][hash(i, key)] += c;
+            const auto h = hash(i, key);
+            m_data[i][h] += c;
         }
     }
 
+    /// \brief Processes a key and reports its new count estimate.
+    /// \param key the key in question
+    /// \param c the number of times to count the key
+    count_t process_and_count(const key_t& key, count_t c = count_t(1)) {
+        count_t min = std::numeric_limits<count_t>::max();
+        for(size_t i = 0; i < m_num_rows; i++) {
+            const auto h = hash(i, key);
+            m_data[i][h] += c;
+            min = std::min(min, m_data[i][h]);
+        }
+        return min;
+    }
+
+    /// \brief Reports a count estimate for the given key.
+    /// \param key the key in question
     count_t count(const key_t& key) const {
         count_t min = std::numeric_limits<count_t>::max();
         for(size_t i = 0; i < m_num_rows; i++) {
-            min = std::min(min, m_data[i][hash(i, key)]);
+            const auto h = hash(i, key);
+            min = std::min(min, m_data[i][h]);
         }
         return min;
     }
