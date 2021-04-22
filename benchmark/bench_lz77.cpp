a52748d3c09b96fc356f05a5986be5ea44d9afb9
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2021-04-22 08:44:05 +0200

    Revive LZQGramTrie

--- benchmark/bench_lz77.cpp
+++ benchmark/bench_lz77.cpp
@@ -28,7 +28,7 @@ using char_t = unsigned char;
 struct {
     std::string filename;
     size_t threshold = 2;
-    size_t q = 8;
+    size_t q = 0;
     
     std::string ds;
     
@@ -81,19 +81,21 @@ int main(int argc, char** argv) {
     bench("base", "Noop", [](){ return Noop<true>(); });
     bench("base", "SA", [](){ return LZ77SA<true>(options.threshold); });
     
-    if(options.q <= 8) {
+    if(options.q == 0 || options.q == 8) {
         bench("btree", "BTree(64),q=8", [](){ return LZQGram<qgram::BTreeProcessor<64, uint64_t>, uint64_t, char_t, std::endian::little, true>(options.threshold); });
         bench("hash", "Hash(40_Mi),q=8", [](){ return LZQGram<qgram::HashProcessor<40_Mi, uint64_t>, uint64_t, char_t, std::endian::little, true>(options.threshold); });
         bench("sketch", "Sketch(f=32Mi,w=2Mi,d=4),q=8", [](){ return LZQGram<qgram::SketchProcessor<32_Mi, 2_Mi, 4, uint64_t>, uint64_t, char_t, std::endian::little, true>(options.threshold); });
         bench("sketch", "Sketch2(f=32Mi,w=2Mi,d=4),q=8", [](){ return LZQGram<qgram::SketchProcessor2<32_Mi, 2_Mi, 4, uint64_t>, uint64_t, char_t, std::endian::little, true>(options.threshold); });
         bench("sketch", "Sketch3(f=32Mi,w=2Mi,d=4),q=8", [](){ return LZQGram<qgram::SketchProcessor3<32_Mi, 2_Mi, 4, uint64_t>, uint64_t, char_t, std::endian::little, true>(options.threshold); });
+        bench("trie", "Trie(MTF),q=8", [](){ return LZQGram<qgram::TrieProcessor<char_t, true>, uint64_t, char_t, std::endian::little, true>(options.threshold); });
     }
     
-    if(options.q <= 16) {
+    if(options.q == 0 || options.q == 16) {
         bench("btree", "BTree(64),q=16", [](){ return LZQGram<qgram::BTreeProcessor<64, uint128_t>, uint128_t, char_t, std::endian::little, true>(options.threshold); });
         bench("hash", "Hash(28_Mi),q=16", [](){ return LZQGram<qgram::HashProcessor<24_Mi, uint128_t>, uint128_t, char_t, std::endian::little, true>(options.threshold); });
         bench("sketch", "Sketch(f=4Mi,w=512Ki,d=4),q=16", [](){ return LZQGram<qgram::SketchProcessor<4_Mi, 512_Ki, 4, uint128_t>, uint128_t, char_t, std::endian::little, true>(options.threshold); });
         bench("sketch", "Sketch2(f=4Mi,w=512Ki,d=4),q=16", [](){ return LZQGram<qgram::SketchProcessor2<4_Mi, 512_Ki, 4, uint128_t>, uint128_t, char_t, std::endian::little, true>(options.threshold); });
         bench("sketch", "Sketch3(f=4Mi,w=512Ki,d=4),q=16", [](){ return LZQGram<qgram::SketchProcessor3<4_Mi, 512_Ki, 4, uint128_t>, uint128_t, char_t, std::endian::little, true>(options.threshold); });
+        bench("trie", "Trie(MTF),q=16", [](){ return LZQGram<qgram::TrieProcessor<char_t, true>, uint128_t, char_t, std::endian::little, true>(options.threshold); });
     }
 }
--- include/tdc/comp/lz77/lzqgram_trie.hpp
+++ include/tdc/comp/lz77/lzqgram_trie.hpp
@@ -6,10 +6,10 @@
 #include <iostream>
 #include <limits>
 #include <stdexcept>
+#include <vector>
 
 #include <tdc/util/index.hpp>
 
-#include "tries.hpp"
 #include "stats.hpp"
 
 namespace tdc {
@@ -17,16 +17,80 @@ namespace comp {
 namespace lz77 {
 namespace qgram {
 
-template<typename trie_t>
+template<typename char_t, bool m_mtf>
 class TrieProcessor {
 private:
-    trie_t m_trie;
+    static constexpr index_t ROOT = 0;
+
+    std::vector<char_t>  m_char;
+    std::vector<index_t> m_first_child;
+    std::vector<index_t> m_next_sibling;
+    std::vector<index_t> m_count;
+    std::vector<index_t> m_last_seen_at;
+    std::vector<index_t> m_shortcut;
+    
+    index_t m_last;
+    
+    index_t get_or_create_child(const index_t node, const char_t c, const index_t at) {
+        const auto first_child = m_first_child[node];
+        auto v = first_child;
+
+        if constexpr(m_mtf) {
+            // MTF
+            auto prev_sibling = ROOT;
+            while(v != ROOT && m_char[v] != c) {
+                prev_sibling = v;
+                v = m_next_sibling[v];
+            }
+            
+            if(v && v != first_child) {
+                m_next_sibling[prev_sibling] = m_next_sibling[v];
+                m_next_sibling[v] = first_child;
+                m_first_child[node] = v;
+            }
+        } else {
+            // FIFO
+            while(v != ROOT && m_char[v] != c) {
+                v = m_next_sibling[v];
+            }
+        }
+        
+        if(v == ROOT) {
+            // child does not exist, create new
+            v = m_char.size();
+            m_char.emplace_back(c);
+            m_first_child.emplace_back(ROOT);
+            m_next_sibling.emplace_back(first_child);
+            m_count.emplace_back(0);
+            m_last_seen_at.emplace_back(0);
+            m_shortcut.emplace_back(ROOT);
+            
+            // insert
+            m_first_child[node] = v;
+        }
+        return v;
+    }
 
 public:
+    TrieProcessor() : m_last(ROOT) {
+        m_char.reserve(16);
+        m_first_child.reserve(16);
+        m_next_sibling.reserve(16);
+        m_count.reserve(16);
+        m_last_seen_at.reserve(16);
+        m_shortcut.reserve(16);
+        
+        // insert root
+        m_char.emplace_back(0);
+        m_first_child.emplace_back(ROOT);
+        m_next_sibling.emplace_back(ROOT);
+        m_count.emplace_back(0);
+        m_last_seen_at.emplace_back(0);
+        m_shortcut.emplace_back(ROOT);
+    }
+
     template<typename lzqgram_t>
     void process(lzqgram_t& c, std::ostream& out) {
-        using char_t = lzqgram_t::char_t;
-        
         // advance in trie as much as possible
         const auto _qgram = c.qgram();
         const char_t* qgram = (const char_t*)&_qgram;
@@ -34,21 +98,37 @@ public:
         size_t len = 0;
         size_t src = 0;
         
-        auto* trie = &m_trie;
-        for(size_t j = 0; j < lzqgram_t::q; j++) {
+        auto current = m_shortcut[m_last];
+        if(current) {
+            // shortcut existed
+            if constexpr(lzqgram_t::track_stats) ++c.stats().num_collisions;
+            
+            len = lzqgram_t::q - 1;
+            src = m_last_seen_at[current];
+            ++m_count[current];
+        }
+        
+        auto parent = current;
+        for(size_t j = len; j < lzqgram_t::q; j++) {
             const size_t i = (lzqgram_t::qgram_endian == std::endian::little) ? lzqgram_t::q - 1 - j : j;
             
-            trie = trie->get_or_create_child(qgram[i]);
-            if(trie->count()) {
+            parent = current;
+            current = get_or_create_child(current, qgram[i], c.pos());
+            if(m_count[current]) {
                 ++len; // nb: this assumes that if a q-gram does not exist in the trie, neither does any (q+1)-gram extending it
-                src = trie->last_seen_at();
+                src = m_last_seen_at[current];
             } else {
                 // node was just created
                 if constexpr(lzqgram_t::track_stats) ++c.stats().trie_size;
             }
             
-            trie->update(trie->count() + 1, c.pos());
+            // update
+            ++m_count[current];
+            m_last_seen_at[current] = c.pos();
         }
+        
+        if(m_last) m_shortcut[m_last] = parent;
+        m_last = current;
         
         c.output_ref(out, src, len);
     }
