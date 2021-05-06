9d6ca7d4352f3743df483d9e30b9fe4acb57009d
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2021-05-06 09:44:10 +0200

    Allow configuration of index_t via CMake

--- include/tdc/util/index.hpp
+++ include/tdc/util/index.hpp
@@ -2,11 +2,17 @@
 
 #include <cstdint>
 
+#ifndef INDEX_T
+#pragma message("The INDEX_T macro isn't defined and will therefore default to uint64_t.")
+/// \brief Determined by the build system.
+#define INDEX_T uint64_t
+#endif
+
 namespace tdc {
 
 /// \brief The type to use for indices into an input.
 ///
 /// This can be used to optimize the memory footprint for applications if it is known that inputs never exceed a certain size.
-using index_t = uint64_t;
+using index_t = INDEX_T;
 
 } // namespace tdc
