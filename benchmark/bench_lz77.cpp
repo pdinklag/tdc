9e062ec342d50660637d3d6da72e27f9c947b4eb
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2021-05-07 08:54:32 +0200

    Allow stating window size in bench_lz77

--- benchmark/bench_lz77.cpp
+++ benchmark/bench_lz77.cpp
@@ -30,6 +30,7 @@ struct {
     std::string filename;
     size_t threshold = 2;
     size_t q = 0;
+    size_t window = 1_Ki;
     
     std::string ds;
     
@@ -50,21 +51,22 @@ void bench(const std::string& group, std::string&& name, ctor_t ctor) {
         Stats stats;
         {
             auto c = ctor();
-            // c.compress(input, std::cout);
-            c.compress(input, devnull);
+            c.compress(input, std::cout);
+            // c.compress(input, devnull);
             stats = c.stats();
         }
         
         auto guard = phase.suppress();
+        if(group == "sliding") phase.log("window", options.window);
         phase.log("input_size", stats.input_size);
         phase.log("num_literals", stats.num_literals);
         phase.log("num_refs", stats.num_refs);
-        phase.log("num_collisions", stats.num_collisions);
-        phase.log("num_extensions", stats.num_extensions);
-        phase.log("extension_sum", stats.extension_sum);
-        phase.log("num_swaps", stats.num_swaps);
+        // phase.log("num_collisions", stats.num_collisions);
+        // phase.log("num_extensions", stats.num_extensions);
+        // phase.log("extension_sum", stats.extension_sum);
+        // phase.log("num_swaps", stats.num_swaps);
         phase.log("trie_size", stats.trie_size);
-        // std::cout << std::endl;
+        std::cout << std::endl;
         std::cout << "RESULT algo=" << name << " group=" << group << " input=" << options.filename << " threshold=" << options.threshold << " " << phase.to_keyval() << std::endl;
     }
 }
@@ -72,9 +74,10 @@ void bench(const std::string& group, std::string&& name, ctor_t ctor) {
 int main(int argc, char** argv) {
     tlx::CmdlineParser cp;
     cp.add_param_string("file", options.filename, "The input file.");
-    cp.add_size_t('t', "threshold", options.threshold, "Minimum reference length.");
+    cp.add_size_t('t', "threshold", options.threshold, "Minimum reference length (default: 2).");
     cp.add_string('a', "group", options.ds, "The algorithm group to benchmark.");
     cp.add_size_t('q', "min-qgram", options.q, "The minimum q-gram length.");
+    cp.add_bytes('w', "window", options.window, "The window length for sliding window algorithms (default: 1024.");
     if(!cp.process(argc, argv)) {
         return -1;
     }
@@ -82,23 +85,23 @@ int main(int argc, char** argv) {
     bench("base", "Noop", [](){ return Noop<true>(); });
     bench("base", "SA", [](){ return LZ77SA<true>(options.threshold); });
     
-    bench("sliding", "Sliding(1_Mi)", [](){ return LZ77SlidingWindow<true>(1_Mi, options.threshold); });
+    bench("sliding", "Sliding", [](){ return LZ77SlidingWindow<true>(options.window, options.threshold); });
     
-    if(options.q == 0 || options.q == 8) {
-        bench("btree", "BTree(64),q=8", [](){ return LZQGram<qgram::BTreeProcessor<64, uint64_t>, uint64_t, char_t, std::endian::little, true>(options.threshold); });
-        bench("hash", "Hash(40_Mi),q=8", [](){ return LZQGram<qgram::HashProcessor<40_Mi, uint64_t>, uint64_t, char_t, std::endian::little, true>(options.threshold); });
-        // bench("sketch", "Sketch(f=32Mi,w=2Mi,d=4),q=8", [](){ return LZQGram<qgram::SketchProcessor<32_Mi, 2_Mi, 4, uint64_t>, uint64_t, char_t, std::endian::little, true>(options.threshold); });
+    // if(options.q == 0 || options.q == 8) {
+        // bench("btree", "BTree(64),q=8", [](){ return LZQGram<qgram::BTreeProcessor<64, uint64_t>, uint64_t, char_t, std::endian::little, true>(options.threshold); });
+        // bench("hash", "Hash(50_Mi),q=8", [](){ return LZQGram<qgram::HashProcessor<50_Mi, uint64_t>, uint64_t, char_t, std::endian::little, true>(options.threshold); });
+        // bench("sketch", "Sketch(f=32Mi,w=4Mi,d=4),q=8", [](){ return LZQGram<qgram::SketchProcessor<32_Mi, 4_Mi, 4, uint64_t>, uint64_t, char_t, std::endian::little, true>(options.threshold); });
         // bench("sketch", "Sketch2(f=32Mi,w=2Mi,d=4),q=8", [](){ return LZQGram<qgram::SketchProcessor2<32_Mi, 2_Mi, 4, uint64_t>, uint64_t, char_t, std::endian::little, true>(options.threshold); });
-        bench("sketch", "Sketch3(f=32Mi,w=2Mi,d=4),q=8", [](){ return LZQGram<qgram::SketchProcessor3<32_Mi, 2_Mi, 4, uint64_t>, uint64_t, char_t, std::endian::little, true>(options.threshold); });
-        bench("trie", "Trie(MTF),q=8", [](){ return LZQGram<qgram::TrieProcessor<char_t, true>, uint64_t, char_t, std::endian::little, true>(options.threshold); });
-    }
+        // bench("sketch", "Sketch3(f=32Mi,w=4Mi,d=4),q=8", [](){ return LZQGram<qgram::SketchProcessor3<32_Mi, 4_Mi, 4, uint64_t>, uint64_t, char_t, std::endian::little, true>(options.threshold); });
+        // bench("trie", "Trie(MTF),q=8", [](){ return LZQGram<qgram::TrieProcessor<char_t, true>, uint64_t, char_t, std::endian::little, true>(options.threshold); });
+    // }
     
-    if(options.q == 0 || options.q == 16) {
-        bench("btree", "BTree(64),q=16", [](){ return LZQGram<qgram::BTreeProcessor<64, uint128_t>, uint128_t, char_t, std::endian::little, true>(options.threshold); });
-        bench("hash", "Hash(28_Mi),q=16", [](){ return LZQGram<qgram::HashProcessor<24_Mi, uint128_t>, uint128_t, char_t, std::endian::little, true>(options.threshold); });
-        // bench("sketch", "Sketch(f=4Mi,w=512Ki,d=4),q=16", [](){ return LZQGram<qgram::SketchProcessor<4_Mi, 512_Ki, 4, uint128_t>, uint128_t, char_t, std::endian::little, true>(options.threshold); });
+    // if(options.q == 0 || options.q == 16) {
+        // bench("btree", "BTree(64),q=16", [](){ return LZQGram<qgram::BTreeProcessor<64, uint128_t>, uint128_t, char_t, std::endian::little, true>(options.threshold); });
+        // bench("hash", "Hash(32_Mi),q=16", [](){ return LZQGram<qgram::HashProcessor<32_Mi, uint128_t>, uint128_t, char_t, std::endian::little, true>(options.threshold); });
+        // bench("sketch", "Sketch(f=4Mi,w=1Mi,d=4),q=16", [](){ return LZQGram<qgram::SketchProcessor<4_Mi, 1_Mi, 4, uint128_t>, uint128_t, char_t, std::endian::little, true>(options.threshold); });
         // bench("sketch", "Sketch2(f=4Mi,w=512Ki,d=4),q=16", [](){ return LZQGram<qgram::SketchProcessor2<4_Mi, 512_Ki, 4, uint128_t>, uint128_t, char_t, std::endian::little, true>(options.threshold); });
-        bench("sketch", "Sketch3(f=4Mi,w=512Ki,d=4),q=16", [](){ return LZQGram<qgram::SketchProcessor3<4_Mi, 512_Ki, 4, uint128_t>, uint128_t, char_t, std::endian::little, true>(options.threshold); });
-        bench("trie", "Trie(MTF),q=16", [](){ return LZQGram<qgram::TrieProcessor<char_t, true>, uint128_t, char_t, std::endian::little, true>(options.threshold); });
-    }
+        // bench("sketch", "Sketch3(f=4Mi,w=1Mi,d=4),q=16", [](){ return LZQGram<qgram::SketchProcessor3<4_Mi, 1_Mi, 4, uint128_t>, uint128_t, char_t, std::endian::little, true>(options.threshold); });
+        // bench("trie", "Trie(MTF),q=16", [](){ return LZQGram<qgram::TrieProcessor<char_t, true>, uint128_t, char_t, std::endian::little, true>(options.threshold); });
+    // }
 }
